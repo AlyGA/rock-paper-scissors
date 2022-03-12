403cfd2b79d9dfb874931ebd8699366d3b074933
Aly Anderson <aly779372@gmail.com>
2022-03-12 14:24:06 -0800

    Add scripting for using buttons

--- script.js
+++ script.js
@@ -1,61 +1,77 @@
-'use strict';
+"use strict";
 
-function computerPlay() { //Function for computer to choose options
-    const options = ["Rock", "Paper", "Scissors"];
-    const random = Math.floor(Math.random() * options.length);
-    return options[random];
-}
-
-let playerWins = "You win!";
-let computerWins = "You lose!"
-
-
-function playRound(playerSelection, computerSelection) { //Outcomes of what the user would choose again the computer.
-    if (playerSelection === "rock" && computerSelection === "Scissors") {
-        return playerWins;
-    } else if (playerSelection === "paper" && computerSelection === "Rock") {
-        return playerWins;
-    } else if (playerSelection === "scissors" && computerSelection === "Paper") {
-        return playerWins;
-    } else if (playerSelection === "scissors" && computerSelection === "Rock") {
-        return computerWins;
-    } else if (playerSelection === "paper" && computerSelection === "Scissors") {
-        return computerWins;
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return computerWins;
-    } else {
-        return "Uh oh! Looks like you typed in something invalid. Please select between the options 'Rock', 'Paper', or 'Scissors.' Thank you!";
-    }
-}
-
-
+// Initial Values
+const body = document.querySelector("body");
+const results = document.querySelector(".results");
+const resultDisplayer = document.createElement("div");
+const overallWinner = document.createElement("div");
+const buttons = document.querySelectorAll(".button");
+const playerWins = "You win!";
+const computerWins = "You lose!";
+const draw = "It's a draw!";
 let playerScore = 0; // Player's starting score
 let computerScore = 0; // Computer's starting score
 
-function game() { // Checks to see which score will be higher than the other's
-    for (let i = 0; i < 5; i++) {
-        const computerSelection = computerPlay(); //Takes the computer's input
-        const playerSelection = window.prompt("Enter your choice: ").toLowerCase(); //Takes the users's input
-        playRound(playerSelection, computerSelection);
-        if (playRound(playerSelection, computerSelection) === playerWins) {
-            console.log("You win this round!");
-            playerScore++;
-        } else {
-            console.log("You lose this round!");
-            computerScore++;
-        }
-        continue;
-     }
-     if (playerScore > computerScore) {
-         console.log("The player wins overall!");
-     } else if (playerScore < computerScore){
-        console.log("Sorry you lose!");
-        return;
-     }
-     else {
-         console.log("It's a draw!");
-         return;
-     }
+//Function for computer to choose options
+function computerPlay() {
+  const options = ["Rock", "Paper", "Scissors"];
+  const random = Math.floor(Math.random() * options.length);
+  return options[random].toLowerCase();
 }
 
-game()
+//Outcomes of what the user would choose again the computer.
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    return playerWins;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return playerWins;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return playerWins;
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return computerWins;
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return computerWins;
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return computerWins;
+  } else if (playerSelection === computerSelection) {
+    return draw;
+  } else {
+    return "Uh oh! Looks like you typed in something invalid. Please select between the options 'Rock', 'Paper', or 'Scissors.' Thank you!";
+  }
+}
+
+// Loops through each button node, since it's a group.
+buttons.forEach((button) => {
+  // Event listener that returns a value after a button is pressed.
+  button.addEventListener("click", function () {
+    const computerSelection = computerPlay(); //Takes the computer's input
+    const playerSelection = window.prompt("Enter your choice: ").toLowerCase(); //Takes the users's input
+    // Plays a round
+    let game = playRound(playerSelection, computerSelection);
+
+    // Displays current result and adds it to the UI.
+    resultDisplayer.classList.add("resultDisplayer");
+    resultDisplayer.textContent = game;
+    results.appendChild(resultDisplayer);
+
+    // Score counting.
+    if (playRound(playerSelection, computerSelection) === playerWins) {
+      playerScore++;
+    } else if (playRound(playerSelection, computerSelection) === computerWins) {
+      computerScore++;
+    }
+
+    // Displays the true winner once either score gets to 5.
+    if (playerScore === 5) {
+      const winnerPlayer = "The player wins overall!";
+      overallWinner.classList.add("overallWinner");
+      overallWinner.textContent = winnerPlayer;
+      body.appendChild(overallWinner);
+    } else if (computerScore === 5) {
+      const winnerComputer = "Sorry you lose!";
+      overallWinner.classList.add("overallWinner");
+      overallWinner.textContent = winnerComputer;
+      results.appendChild(overallWinner);
+    }
+  });
+});
